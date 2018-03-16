db540580ba5475f4fb5e4043ae259443cab8a282
saibot777 <stefantrajkovic777@gmail.com>
2018-03-16 10:54:49 +0100

    user model update - gravatar method

--- server/src/models/user.model.ts
+++ server/src/models/user.model.ts
@@ -1,6 +1,7 @@
 import * as mongoose from 'mongoose';
 const bcrypt = require('bcrypt'), SALT_WORK_FACTOR = 10;
 import {NextFunction} from "express";
+const crypto = require('crypto');
 
 const UserSchema = new mongoose.Schema({
     email: { type: String, unique: true, lowercase: true },
@@ -38,4 +39,16 @@ UserSchema.pre('save', (next: NextFunction) => {
 });
 
 UserSchema.methods.comparePassword = password => bcrypt.compareSync(password, this.password);
+
+UserSchema.methods.gravatar = size => {
+    if (!this.size) size = 200;
+
+    if (!this.email) {
+        return 'https://gravatar.com/avatar/?s' + size + '&d=retro';
+    } else {
+        let md5 = crypto.createHash('md5').update(this.email).digest('hex');
+        return 'https://gravatar.com/avatar/' + md5 + '?s' + size + '&d=retro';
+    }
+    
+};
 
